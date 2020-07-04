d59df4566a50f88850c5baf019480f72fab0e3ee
Tobi Olabode <uncletobi_0615@outlook.com>
2020-07-04 20:01:47 +0100

    Fixing Trends not working on Ubuntu4

--- selchartsng/selchartsng/settings.py
+++ selchartsng/selchartsng/settings.py
@@ -42,22 +42,22 @@ ROBOTSTXT_OBEY = False
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-# DEFAULT_REQUEST_HEADERS = {  
-#     'Method':'GET',
-#     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-#     'Accept-Encoding': 'gzip, deflate, br',
-#     'Accept-Language': 'en-US,en;q=0.9',
-#     'Authorization': 'Bearer',
-#     'Sec-Fetch-Dest': 'empty',
-#     'Sec-Fetch-Mode': 'cors',
-#     'Sec-Fetch-Site': 'same-origin',
-#     'Dnt': '1',
-#     'referer': 'https://www.google.com.ng/',
-#     'Sec-Fetch-Dest': 'document',
-#     'Sec-Fetch-Mode': 'navigate',
-#     'Sec-Fetch-Site': 'none',
-#     'Sec-Fetch-User': '?1',
-# }
+DEFAULT_REQUEST_HEADERS = {  
+    'Method':'GET',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Authorization': 'Bearer',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'Dnt': '1',
+    'referer': 'https://www.google.com.ng/',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1',
+}
 
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
